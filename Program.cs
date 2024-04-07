656e400d109c2c634a6dc6c875d6edd07bed6bf2
NewtonRelevantz <166278299+NewtonRelevantz@users.noreply.github.com>
2024-04-07 12:14:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using EmailSenderProgram.Common;
+using System;
+using EmailSenderProgram.Repository.Interface;
+using EmailSenderProgram.Repository.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailSenderProgram
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// This application is run everyday
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            LogHelper.LogInformation("Email Trigger Initiated.");
+            try
+            {
+                var serviceProvider = new ServiceCollection()
+            .AddTransient<ICustomerEmail, CustomerEmail>()
+            .BuildServiceProvider();
+                var customerService = serviceProvider.GetRequiredService<ICustomerEmail>();
+                LogHelper.LogInformation("Email Trigger CustomerCreateAlert Initiated.");
+                customerService.CustomerCreateAlert();
+                LogHelper.LogInformation("Email Trigger CustomerCreateAlert ended.");
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    LogHelper.LogInformation("Email Trigger ComeBackMail Initiated.");
+                    customerService.ComeBackMail();
+                    LogHelper.LogInformation("Email Trigger ComeBackMail ended.");
+                }
+                LogHelper.LogInformation("Email Trigger ended.");
+            }
+
+            catch (Exception ex)
+            {
+                LogHelper.LogError($"{ex.Message}=> Stack Trace=>{ex.StackTrace}");
+            }
+        }
+
+
+    }
+}
