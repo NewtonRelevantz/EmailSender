e9671817fc44fdc456ac94a92140f1dbc75428a1
NewtonRelevantz <166278299+NewtonRelevantz@users.noreply.github.com>
2024-04-07 12:19:10 +0530

    Add files via upload

---
+++ Common/EmailHelper.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace EmailSenderProgram.Common
+{
+    public static class EmailHelper
+    {
+        public static void SendEmail(List<string> To, List<string> CC, string subject, string body)
+        {
+            try
+            {
+                LogHelper.LogInformation("EmailHelper=>SendEmail=> Method Initiated.");
+                if (To != null && To.Any())
+                {
+                    To.ForEach(e =>
+                    {
+                        if (e.IsValidEmail())
+                        {
+                            //Create a new MailMessage
+                            System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage();
+                            //Add customer to reciever list
+                            m.To.Add("newtonselvam@gmail.com");
+                            if (CC != null && CC.Any())
+                            {
+                                CC.ForEach(p =>
+                                {
+                                    if (e.IsValidEmail())
+                                    {
+                                        m.CC.Add(p);
+                                    }
+                                    else
+                                    {
+                                        LogHelper.LogInformation($"EmailHelper=>SendEmail=> one of the CC email is empty or not valid=> EmailId=>{e}");
+                                    }
+                                });
+                            }
+                            //Add subject
+                            m.Subject = subject;
+                            //Send mail from info@EO.com
+                            m.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings.GetValues("senderemail")?.FirstOrDefault()?.ToString());
+                            //Add body to mail
+                            m.Body = "Hi " + e.Split('@')?.FirstOrDefault() +
+                                     "<br>" + body + "<br><br>Best Regards,<br>EO Team";
+                            m.IsBodyHtml = true;
+                            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings.GetValues("smptserver")?.FirstOrDefault()?.ToString());
+                            client.Port = int.Parse(ConfigurationManager.AppSettings.GetValues("smptport")?.FirstOrDefault()?.ToString());
+                            client.UseDefaultCredentials = false;
+                            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings.GetValues("senderemail")?.FirstOrDefault()?.ToString(), "gxgo gkju gfys xcso");
+                            client.EnableSsl = true;
+                            //Send mail
+                            client.Send(m);
+                            client.Dispose();
+                            LogHelper.LogInformation($"EmailHelper=>SendEmail=> Mail send successfully=> EmailId=>{e}");
+                        }
+                        else
+                        {
+                            LogHelper.LogInformation($"EmailHelper=>SendEmail=> one of the TO email is empty or not valid=> EmailId=>{e}");
+                        }
+
+
+                    });
+                }
+                else
+                {
+                    LogHelper.LogInformation("EmailHelper=>SendEmail=> unable to send email because of no receipients are found");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                LogHelper.LogError($"{ex.Message}=> Stack Trace=>{ex.StackTrace}");
+                throw;
+            }
+        }
+
+        private static bool IsValidEmail(this string email)
+        {
+            // Regular expression pattern for email validation
+            string pattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+            // Use Regex.IsMatch to check if the email matches the pattern
+            return Regex.IsMatch(email, pattern);
+        }
+    }
+}
---
+++ Common/LogHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace EmailSenderProgram.Common
+{
+    public static class LogHelper
+    {
+        private static string LogPath = $@"log\{DateTime.Now.ToString("yyyyMMdd")}.txt";
+        public static void LogInformation(string Message)
+        {
+            checkLog();
+            using (StreamWriter writer = File.AppendText(LogPath))
+            {
+                writer.WriteLine($"{DateTime.Now}: {Message}");
+            }
+        }
+
+
+        public static void LogError(string Message)
+        {
+            checkLog();
+            using (StreamWriter writer = File.AppendText(LogPath))
+            {
+                writer.WriteLine($"Failed at {DateTime.Now}: {Message}");
+            }
+        }
+
+        private static void checkLog()
+        {
+            if (!File.Exists(LogPath))
+            {
+                var dir = System.IO.Path.GetDirectoryName(
+           System.Reflection.Assembly.GetEntryAssembly().Location);
+                string folderPath = System.IO.Path.Combine(dir, "log");
+
+                // Check if the folder already exists
+                if (!Directory.Exists(folderPath))
+                {
+                    // Create the folder
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                using (StreamWriter writer = File.CreateText(LogPath))
+                {
+                }
+
+            }
+        }
+    }
+}
